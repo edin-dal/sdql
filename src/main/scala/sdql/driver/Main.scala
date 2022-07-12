8002afaec236b6ad718b199bf5b21737ec0df188
Amir Shaikhha <amir.shaikhha@epfl.ch>
2022-07-12 23:03:50 +0100

    Main driver added

---
+++ src/main/scala/sdql/driver/Main.scala
@@ -0,0 +1,25 @@
+package sdql
+package driver
+import frontend._
+import backend._
+import ir._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    if(args.length < 1) {
+      raise("usage: `run <cmd> <args>*`")
+    }
+    args(0) match {
+      case "interpret" =>
+        if(args.length != 2) {
+          raise("usage: `run interpret <sdql_file>`")
+        }
+        val filename = args(1)
+        val prog = SourceCode.fromFile(filename).exp
+        val res = Interpreter(prog)
+        println(Value.toString(res))
+      case arg =>
+        raise(s"`run $arg` not supported")
+    }
+  }
+}
