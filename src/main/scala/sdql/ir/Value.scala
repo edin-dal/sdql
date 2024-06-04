5bb26222d8bdef6864931979a0194bd6bc4efbf8
amascolo <12738821+amascolo@users.noreply.github.com>
2024-06-04 22:08:34 +0100

    Improved printing value

--- src/main/scala/sdql/ir/Value.scala
+++ src/main/scala/sdql/ir/Value.scala
@@ -9,7 +9,10 @@ case object ZeroValue
 object Value {
   def toString(v: Value): String = v match {
     case b: Boolean => b.toString
-    case s: String => "\"" + s.toString + "\""
+    case DateValue(v) =>
+      val s = v.toString
+      s"${s.substring(0, 4)}-${s.substring(4, 6)}-${s.substring(6, 8)}"
+    case s: String => "\"" + s + "\""
     case d: Double => d.toString
     case i: Int => i.toString
     case m: Map[_, _] => 
@@ -17,7 +20,9 @@ object Value {
         s"${toString(kv._1)} -> ${toString(kv._2)}"
       ).mkString("{", ", ", "}")
     case RecordValue(vals) =>
-      vals.map(fv => s"${fv._1} = ${fv._2}").mkString("<", ", ", ">")
+      vals.map(fv => s"${toString(fv._1)} = ${toString(fv._2)}").mkString("<", ", ", ">")
+    case ZeroValue =>
+      ZeroValue.toString
     case _ => raise(s"Doesn't know how to convert `$v` to string")
   }
   def normalize(v: Value): Value = 
