d5217b085279b0e69560cb2dad479635326265a8
amascolo <12738821+amascolo@users.noreply.github.com>
2024-06-15 13:53:42 +0100

    SourceCode close file

--- src/main/scala/sdql/frontend/SourceCode.scala
+++ src/main/scala/sdql/frontend/SourceCode.scala
@@ -7,7 +7,8 @@ class SourceCode(val fileName: String, val exp: Exp)
 
 object SourceCode {
   def fromFile(fileName: String): SourceCode = {
-    val content = scala.io.Source.fromFile(fileName).mkString
+    val source = scala.io.Source.fromFile(fileName)
+    val content = try source.mkString finally source.close()
     new SourceCode(fileName, Parser(content))
   }
 }
