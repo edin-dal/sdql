a64b89de680b968a44027c1b3c225f25ee7f9adb
amascolo <12738821+amascolo@users.noreply.github.com>
2024-07-17 12:11:25 +0100

    JOB 18

--- src/test/scala/sdql/backend/CppCodegenTest.scala
+++ src/test/scala/sdql/backend/CppCodegenTest.scala
@@ -317,6 +317,7 @@ sum(<x_s, x_s_v> <- S)
     compilesFile("progs/job/gj/17e.sdql")
     compilesFile("progs/job/gj/17f.sdql")
   }
+//  FIXME
 //  it should "codegen JOB 18" in {
 //    compilesFile("progs/job/gj/18a.sdql")
 //    compilesFile("progs/job/gj/18b.sdql")
