ba648bad924acd15ef72e9120c3938c790102044
amascolo <12738821+amascolo@users.noreply.github.com>
2024-07-20 18:07:58 +0100

    Scatter plot

--- benchmarks/job_gj.py
+++ benchmarks/job_gj.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 from typing import Any, Final
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 FILE_DIR: Final[str] = os.path.dirname(os.path.realpath(__file__))
@@ -72,12 +73,27 @@ def get_ms(s: str, regex: re.Pattern) -> int:
     return round(float(regex.search(s).group(1)))
 
 
+def plot(df: pd.DataFrame) -> None:
+    plt.plot(df[df.columns[0]], df[df.columns[0]], color="gray")
+    plt.scatter(df[df.columns[0]], df[df.columns[1]], color="orange", s=10)
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Expected (ms)")
+    plt.ylabel("Actual (ms)")
+    plt.show()
+
+
 if __name__ == "__main__":
     job_df = read_job_results()
     wcoj_df = read_wcoj_results()
     df = pd.merge(
         wcoj_df, job_df, how="outer", on="Query", suffixes=(" expected", " actual")
     ).set_index("Query")
-    df["Diff (ms)"] = df.iloc[:, 1] - df.iloc[:, 0]
-    df["Diff (%)"] = (100 * df["Diff (ms)"] / df.iloc[:, 0]).round().astype(int)
+    df["Diff (ms)"] = df[df.columns[1]] - df[df.columns[0]]
+    df["Diff (%)"] = (100 * df["Diff (ms)"] / df[df.columns[0]]).round().astype(int)
     print(df.sort_values(by=df.columns[3], ascending=False))
+    print()
+    print(f"{df.columns[3]} summary")
+    pct_diff_summary = df[df.columns[3]].describe().round().astype(int)
+    print(pct_diff_summary)
+    plot(df)
