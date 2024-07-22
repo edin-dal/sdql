27cd0d10d573862ca9c70dc19d34d3b6b37bc83a
amascolo <12738821+amascolo@users.noreply.github.com>
2024-07-22 13:34:20 +0100

    Much faster codegen in batch for JOB queries

--- benchmarks/scripts/codegen_job.sh
+++ benchmarks/scripts/codegen_job.sh
@@ -6,10 +6,5 @@ for file in progs/job/gj/*.sdql; do
   rm -f generated/$no_ext.cpp
 done
 
-# TODO codegen in batch like codegen_tpch.sh
-for file in progs/job/gj/*.sdql; do
-  name=${file##*/}
-  no_ext="${name%.*}"
-  echo "Generating $no_ext"
-  sbt "run benchmark $1 progs/job/gj $name"
-done
+qs=( $(ls progs/job/gj/ | grep '.sdql' ) )
+sbt "run benchmark $1 progs/job/gj $(printf '%q ' "${qs[@]}")"
