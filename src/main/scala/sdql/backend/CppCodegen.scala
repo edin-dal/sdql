2d85ca984fd5408e0927ff07686a1a50e96e4459
amascolo <12738821+amascolo@users.noreply.github.com>
2024-07-25 17:16:44 +0100

    Avoid nested std::string initialisation in tuples

--- src/main/scala/sdql/backend/CppCodegen.scala
+++ src/main/scala/sdql/backend/CppCodegen.scala
@@ -561,7 +561,7 @@ object CppCodegen {
     case BoolType => "false"
     case RealType => "0.0"
     case IntType | DateType => "0"
-    case StringType(None) => "std::string()"
+    case StringType(None) => "\"\""
     case StringType(Some(_)) => raise("initialising VarChars shouldn't be needed")
     case DictType(_, _, DictNoHint()) => "{}"
     case DictType(_, _, DictVectorHint()) => vecSize.toString
