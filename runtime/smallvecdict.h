43d1ecf3c5755f4eb35681dec5828d2914d46baf
amascolo <12738821+amascolo@users.noreply.github.com>
2024-08-20 20:54:49 +0100

    Clang-tidy cleanup to smallvecdict

--- runtime/smallvecdict.h
+++ runtime/smallvecdict.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <array>
-#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -15,9 +14,9 @@ class smallvec {
 public:
 	~smallvec() { delete heap_; }
 
-	inline size_t size() const { return size_; }
+	[[nodiscard]] size_t size() const { return size_; }
 
-	inline void push_back(const T &value) {
+	void push_back(const T &value) {
 		if (size_ < N) {
 			stack_[size_] = value;
 		} else {
@@ -29,80 +28,75 @@ public:
 		++size_;
 	}
 
-	inline T &operator[](size_t pos) {
+	T &operator[](size_t pos) {
 		if (size_ <= N) {
 			return stack_[pos];
-		} else {
-			return heap_->at(pos);
 		}
+		return heap_->at(pos);
 	}
 
 	class iterator {
 		T *ptr_;
 
 	public:
-		iterator(T *ptr) : ptr_(ptr) {}
+		explicit iterator(T *ptr) : ptr_(ptr) {}
 
 		iterator &operator++() {
 			++ptr_;
 			return *this;
 		}
 
-		inline T &operator*() { return *ptr_; }
+		T &operator*() { return *ptr_; }
 
-		inline bool operator==(const iterator &rhs) { return ptr_ == rhs.ptr_; }
+		bool operator==(const iterator &rhs) { return ptr_ == rhs.ptr_; }
 
-		inline bool operator!=(const iterator &rhs) { return ptr_ != rhs.ptr_; }
+		bool operator!=(const iterator &rhs) { return ptr_ != rhs.ptr_; }
 	};
 
 	class const_iterator {
 		T *ptr_;
 
 	public:
-		const_iterator(T *ptr) : ptr_(ptr) {}
+		explicit const_iterator(T *ptr) : ptr_(ptr) {}
 
 		const_iterator &operator++() {
 			++ptr_;
 			return *this;
 		}
 
-		inline const T &operator*() { return *ptr_; }
+		const T &operator*() { return *ptr_; }
 
-		inline bool operator==(const const_iterator &rhs) { return ptr_ == rhs.ptr_; }
+		bool operator==(const const_iterator &rhs) { return ptr_ == rhs.ptr_; }
 
-		inline bool operator!=(const const_iterator &rhs) { return ptr_ != rhs.ptr_; }
+		bool operator!=(const const_iterator &rhs) { return ptr_ != rhs.ptr_; }
 	};
 
-	inline iterator begin() {
+	iterator begin() {
 		if (size_ <= N) {
 			return iterator(stack_.data());
-		} else {
-			return iterator(heap_->data());
 		}
+		return iterator(heap_->data());
 	}
 
-	inline iterator end() {
+	iterator end() {
 		if (size_ <= N) {
 			return iterator(stack_.data() + size_);
-		} else {
-			return iterator(heap_->data() + size_);
 		}
+		return iterator(heap_->data() + size_);
 	}
 
-	inline const_iterator begin() const {
+	[[nodiscard]] const_iterator begin() const {
 		if (size_ <= N) {
 			return const_iterator(stack_.data());
-		} else {
-			return const_iterator(heap_->data());
 		}
+		return const_iterator(heap_->data());
 	}
 
-	inline const_iterator end() const {
+	[[nodiscard]] const_iterator end() const {
 		if (size_ <= N) {
 			return const_iterator(stack_.data() + size_);
-		} else {
-			return const_iterator(heap_->data() + size_);
 		}
+		return const_iterator(heap_->data() + size_);
 	}
 };
 
@@ -120,26 +114,26 @@ public:
     // public so smallvecdicts can access it
 	Container svec_;
 
-	inline size_t size() const { return svec_.size(); }
+	[[nodiscard]] size_t size() const { return svec_.size(); }
 
 	class Proxy {
-		smallvecdict<T, N> &svecdict_;
+		smallvecdict &svecdict_;
 		T key_;
 
 	public:
-		Proxy(smallvecdict &svecdict, T key) : svecdict_(svecdict), key_(key) {}
+		Proxy(smallvecdict &svecdict, T key) : svecdict_(svecdict), key_(std::move(key)) {}
 
-		inline Proxy &operator+=(bool) {
+		Proxy &operator+=(int) {
 			svecdict_.svec_.push_back(key_);
 			return *this;
 		}
 	};
 
-	inline Proxy operator[](T key) {
+	Proxy operator[](T key) {
 		return Proxy(*this, key);
 	}
 
-	inline typename Container::iterator begin() { return svec_.begin(); }
+	typename Container::iterator begin() { return svec_.begin(); }
 
-	inline typename Container::iterator end() { return svec_.end(); }
+	typename Container::iterator end() { return svec_.end(); }
 };
