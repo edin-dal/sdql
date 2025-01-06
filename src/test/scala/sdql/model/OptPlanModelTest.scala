3edcd0c73eefe005d2a0ba77ac323768ee9c09f9
amascolo <12738821+amascolo@users.noreply.github.com>
2025-01-06 18:14:00 +0000

    Moved out plan from path

--- src/test/scala/sdql/model/OptPlanModelTest.scala
+++ src/test/scala/sdql/model/OptPlanModelTest.scala
@@ -1,11 +1,7 @@
 package sdql.model
 
-import io.circe.DecodingFailure
-import io.circe.generic.extras.auto.*
-import io.circe.parser.*
 import org.scalatest.ParallelTestExecution
 import org.scalatest.flatspec.AnyFlatSpec
-import sdql.model.CirceConfig.*
 
 import java.nio.file.{ Files, Paths }
 import scala.jdk.CollectionConverters.*
@@ -16,25 +12,9 @@ class OptPlanModelTest extends AnyFlatSpec with ParallelTestExecution {
 
   it should "decode JOB GJ json plans" in decodeJobJsonPlans("plans/job/gj")
 
-  def decodeJobJsonPlans(path: String): Unit = {
-    System.getProperty("user.dir")
+  private def decodeJobJsonPlans(path: String): Unit = {
     val paths     = Paths.get(System.getProperty("user.dir")).resolve(path)
     val jsonFiles = Files.list(paths).iterator().asScala.filter(_.toString.endsWith(".json"))
-
-    jsonFiles.foreach { path =>
-      val source     = scala.io.Source.fromFile(path.toFile)
-      val jsonString =
-        try source.mkString
-        finally source.close()
-
-      val decodedOptPlan = decode[OptPlanModel](jsonString)
-
-      decodedOptPlan match {
-        case Left(e: DecodingFailure) =>
-          fail(s"Failed to parse JSON from $path: ${e.message} at ${e.history.map(_.toString).mkString(" -> ")}")
-        case Left(e)                  => fail(s"Failed to parse JSON from $path: $e")
-        case Right(_)                 =>
-      }
-    }
+    jsonFiles.foreach(path => OptPlanModel.fromPath(path))
   }
 }
